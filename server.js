6b504006cd233b3314810a3573f1ed8b97b0e7a6
Pradip P Caulagi <caulagi@gmail.com>
2012-11-10 11:01:40 +0530

    Initial folder structure

--- server.js
+++ server.js
@@ -1,7 +1,49 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8000);
+var express = require('express')
+    , stylus = require('stylus')
+    , passport = require('passport')
+    , util = require('util')
+    , GitHubStrategy = require('passport-github').Strategy;
 
-console.log('Server running at http://0.0.0.0:8000/');
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .set('warn', true)
+    .set('compress', true);
+}
+
+var app = module.exports = express();
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'htuayreve'}));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(app.router);
+
+  app.use(stylus.middleware({
+      src: __dirname + '/views' // .styl files are located in `views/stylesheets`
+    , dest: __dirname + '/static' // .styl resources are compiled `/stylesheets/*.css`
+    , compile: compile
+  }));
+  app.use(express.static(__dirname + '/static'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', function(req, res, next) {
+  res.render('index', {
+    title: 'Welcome to github-messenging'
+  });
+});
+
+app.listen(3000)
+console.info("Started on port 3000");
