58a27cc8c55ef93f86c5e320447528e9c5992b4f
Pradip P Caulagi <caulagi@gmail.com>
2012-11-12 01:09:36 +0530

    handlers to send message

--- static/js/github-messaging.js
+++ static/js/github-messaging.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var messageTo
+  var messageTo = undefined
     ;
 
   $('#friends ul li').on('click', function(e) {
@@ -8,6 +8,33 @@ $(document).ready(function() {
       $(this).removeClass('active');
     });
     messageTo = $(this).text();
+    // Get the github id of this user
+    for (var i = 0; i < GM.following.length; i++) {
+      if ( GM.following[i].login === messageTo ) {
+        messageTo = GM.following[i].id;
+      }
+    }
     $(this).toggleClass('active');
+  });
+
+  $('#messages form button').on('click', function(e) {
+    e.preventDefault();
+    if ( messageTo === undefined ) {
+      return false;
+    }
+
+    var message = $("#messages form input[name='new_message']").val();
+    if ( message === undefined || message === '' ) {
+      return false;
+    }
+
+    $.ajax({
+      type: "GET",
+      url: "/message/add",
+      data: { from: GM.user.id, to: messageTo, message: message },
+      success: function(data) { 
+        console.log(data);
+      }
+    });
   });
 });
